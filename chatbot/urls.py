c439f3284b1616cca640baa163008e494185db65
AMST0 <52761210+AMST0@users.noreply.github.com>
2023-10-30 23:26:02 +0300

    .

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chatbot, name='chatbot'),
+    path('main_screen/', views.chatbot, name='chatbot'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
     path('about/',views.about,name ="about"),
-    path('main_screen/',views.main_screen,name ="main_screen"),
+    path('',views.main_screen,name ="main_screen"),
 ]
